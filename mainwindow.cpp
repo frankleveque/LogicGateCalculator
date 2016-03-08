5dc0bfe5594df4215418d1569003348c58990a9b
frankleveque <leveque.frank@gmail.com>
2016-03-08 02:29:05 -0700

    fix indent

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,37 +20,32 @@ MainWindow::~MainWindow()
 
 void MainWindow::setAnd(QByteArray a, QByteArray b){
     ui->andEdit->setText(QString("0b") +
-                         QString::number(
-                             a.toULong(&isError, 2)  &
-                             b.toULong(&isError, 2), 2));
+            QString::number(
+                a.toULong(&isError, 2)  &
+                b.toULong(&isError, 2), 2));
 }
-
 
 void MainWindow::setOr(QByteArray a, QByteArray b){
     ui->orEdit->setText(QString("0b") + QString::number(
-                            a.toULong(&isError, 2) |
-                            b.toULong(&isError, 2), 2));
+                a.toULong(&isError, 2) |
+                b.toULong(&isError, 2), 2));
 }
-
 
 void MainWindow::setNot(const QByteArray a, const QByteArray b){
 
     ui->notAEdit->setText(QString("0b") + QString::number(
-                              a.toULong(&isError, 2) ^ QByteArray(a.size(),'1').toULong(&isError,2), 2));
+                a.toULong(&isError, 2) ^ QByteArray(a.size(),'1').toULong(&isError,2), 2));
 
     ui->notBEdit->setText(QString("0b") + QString::number(
-                              b.toULong(&isError, 2) ^ QByteArray(b.size(),'1').toULong(&isError,2), 2));
-
+                b.toULong(&isError, 2) ^ QByteArray(b.size(),'1').toULong(&isError,2), 2));
 
 }
 
 void MainWindow::setNand(const QByteArray a, const QByteArray b){
     //not both then or
-    ui->nandEdit->setText(QString("0b") +
-                          QString::number(
-                              (a.toULong(&isError, 2) ^ QByteArray(a.size(),'1').toULong(&isError,2)) |
-                              (b.toULong(&isError, 2) ^ QByteArray(b.size(),'1').toULong(&isError,2))
-                              , 2));
+    ui->nandEdit->setText(QString("0b") + QString::number(
+                (a.toULong(&isError, 2) ^ QByteArray(a.size(),'1').toULong(&isError,2)) |
+                (b.toULong(&isError, 2) ^ QByteArray(b.size(),'1').toULong(&isError,2)), 2));
 
 }
 
@@ -58,53 +53,48 @@ void MainWindow::setNor(const QByteArray a, const QByteArray b){
     // or both then not result
     int orLength = (a.size() < b.size())? b.size() : a.size();
     ui->norEdit->setText(QString("0b") +
-                         QString::number(
-                             (a.toULong(&isError, 2) | b.toULong(&isError, 2)) ^ QByteArray(orLength,'1').toULong(&isError,2)
-                             , 2));
+            QString::number(
+                (a.toULong(&isError, 2) | b.toULong(&isError, 2)) ^ 
+                QByteArray(orLength,'1').toULong(&isError,2), 2));
 }
 
 void MainWindow::setXor(QByteArray a, QByteArray b){
     ui->xorEdit->setText(QString("0b") + QString::number(
-                             a.toULong(&isError, 2) ^
-                             b.toULong(&isError, 2), 2));
+                a.toULong(&isError, 2) ^
+                b.toULong(&isError, 2), 2));
 }
-
 
 void MainWindow::setXnor(QByteArray a, QByteArray b){
     int xnorLength = (a.size() < b.size())? b.size() : a.size();
     ui->xnorEdit->setText(QString("0b") + QString::number(
-                              (a.toULong(&isError, 2) ^ b.toULong(&isError, 2)) ^
-                              QByteArray(xnorLength,'1').toULong(&isError,2), 2));
+                (a.toULong(&isError, 2) ^ b.toULong(&isError, 2)) ^
+                QByteArray(xnorLength,'1').toULong(&isError,2), 2));
 }
-
-
-
-
 
 void MainWindow::processInputs(){
     if(ui->aEdit->text().isEmpty()){
-            ui->aBinary->clear();
-            ui->andEdit->clear();
-            ui->orEdit->clear();
-            ui->notAEdit->clear();
-            ui->notBEdit->clear();
-            ui->nandEdit->clear();
-            ui->norEdit->clear();
-            ui->xorEdit->clear();
-            ui->xnorEdit->clear();
-            return;
-        } else if(ui->bEdit->text().isEmpty()){
-            ui->bBinary->clear();
-            ui->andEdit->clear();
-            ui->orEdit->clear();
-            ui->notAEdit->clear();
-            ui->notBEdit->clear();
-            ui->nandEdit->clear();
-            ui->norEdit->clear();
-            ui->xorEdit->clear();
-            ui->xnorEdit->clear();
-            return;
-        }
+        ui->aBinary->clear();
+        ui->andEdit->clear();
+        ui->orEdit->clear();
+        ui->notAEdit->clear();
+        ui->notBEdit->clear();
+        ui->nandEdit->clear();
+        ui->norEdit->clear();
+        ui->xorEdit->clear();
+        ui->xnorEdit->clear();
+        return;
+    } else if(ui->bEdit->text().isEmpty()){
+        ui->bBinary->clear();
+        ui->andEdit->clear();
+        ui->orEdit->clear();
+        ui->notAEdit->clear();
+        ui->notBEdit->clear();
+        ui->nandEdit->clear();
+        ui->norEdit->clear();
+        ui->xorEdit->clear();
+        ui->xnorEdit->clear();
+        return;
+    }
 
     long aText = ui->aEdit->text().toLong();
     long bText = ui->bEdit->text().toLong();
--- mainwindow.h
+++ mainwindow.h
@@ -10,24 +10,24 @@ namespace Ui {
 
 class MainWindow : public QMainWindow
 {
-private:
-    Q_OBJECT
-    Ui::MainWindow *ui;
-    bool isError;
-    void setAnd(const QByteArray a, const QByteArray b);
-    void setOr(const QByteArray a, const QByteArray b);
-    void setNot(const QByteArray a, const QByteArray b);
-    void setNand(const QByteArray a, const QByteArray b);
-    void setNor(const QByteArray a, const QByteArray b);
-    void setXor(const QByteArray a, const QByteArray b);
-    void setXnor(const QByteArray a, const QByteArray b);
+    private:
+        Q_OBJECT
+            Ui::MainWindow *ui;
+        bool isError;
+        void setAnd(const QByteArray a, const QByteArray b);
+        void setOr(const QByteArray a, const QByteArray b);
+        void setNot(const QByteArray a, const QByteArray b);
+        void setNand(const QByteArray a, const QByteArray b);
+        void setNor(const QByteArray a, const QByteArray b);
+        void setXor(const QByteArray a, const QByteArray b);
+        void setXnor(const QByteArray a, const QByteArray b);
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    public:
+        explicit MainWindow(QWidget *parent = 0);
+        ~MainWindow();
 
-public slots:
-    void processInputs();
+        public slots:
+            void processInputs();
 
 };
 #endif
